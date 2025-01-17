5c4277b17460a02d4862efe157353ad12e314ce4
Jose Manuel <jmsg00702@gmail.com>
2025-01-17 09:38:28 +0100

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   // process requiere npm install -D @types/node
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    base: env.VITE_APP_BASE_URL || '/DIRI/',
+    base: env.VITE_APP_BASE_URL || '/',
     build:{
       outDir:'docs',
     },
